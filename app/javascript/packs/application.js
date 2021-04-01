72290a3c20b56e3d5ba6248bddd3fa489acafe88
Sergei_Savelev <lavrentiy83@gmail.com>
2021-04-01 03:25:26 +0300

    editing folder structure

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,16 +1,14 @@
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/frontend and only use these pack files to reference
+// a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
 // import * as ActiveStorage from "@rails/activestorage"
-// import "channels"
+import "channels"
 
 Rails.start()
 // ActiveStorage.start()
 
-import 'bootstrap'
-import '../styles/application'
-
-const images = require.context("../images", true)
+import 'bootstrap/dist/js/bootstrap'
+import '../../assets/stylesheets/application.scss'
