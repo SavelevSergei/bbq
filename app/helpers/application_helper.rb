84127841582e5e62519dd2ce1de210bdcd73df93
Sergei_Savelev <lavrentiy83@gmail.com>
2021-04-11 12:40:47 +0300

    add comment&subscriptions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,4 +22,8 @@ module ApplicationHelper
   def user_avatar(user)
     asset_path('bender.png')
   end
+
+  def fa_icon(icon_class)
+    content_tag 'span', '', class: "fa fa-#{icon_class}"
+  end
 end
