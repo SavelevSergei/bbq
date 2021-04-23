a71592f6c672dbda0047223df7e6871f613835b7
Sergei_Savelev <lavrentiy83@gmail.com>
2021-04-23 03:59:01 +0300

    del author comment from mailing

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,7 +74,7 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
 
