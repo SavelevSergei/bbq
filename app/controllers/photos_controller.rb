bab8be279216c815c50c658b4a3f7c612408402b
Sergei_Savelev <lavrentiy83@gmail.com>
2021-04-23 17:30:26 +0300

    fix bug for no name user

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -43,7 +43,7 @@ class PhotosController < ApplicationController
 
   def notify_photo(event, photo)
     # собираем всех подписчиков и автора события в массив мэйлов, исключаем повторяющиеся
-    all_emails = (event.subscriptions.map(&:user_email) + [event.user.email] - [photo.user.email]).uniq
+    all_emails = (event.subscriptions.map(&:user_email) + [event.user.email] - [photo.user&.email]).uniq
 
     # XXX: Этот метод может выполняться долго из-за большого числа подписчиков
     # поэтому в реальных приложениях такие вещи надо выносить в background задачи!
